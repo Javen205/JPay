bdbe820bca62c5518e42ab081582c2f3710cb95c
Javen205 <javen205@126.com>
2017-07-11 18:24:32 +0800

    修改报名为com.jpay

---
+++ jpaylib/src/main/java/com/jpay/utils/MD5.java
@@ -0,0 +1,36 @@
+package com.jpay.utils;
+
+import java.security.MessageDigest;
+
+public class MD5 {
+    public static String MD5sign(String s) {
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        try {
+            byte[] btInput = s.getBytes("UTF-8");
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str).toLowerCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+	public static void main(String[] args) {
+		System.out.println(MD5sign("Hello world"));
+	}
+}
